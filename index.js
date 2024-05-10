6410a4f30482803b3caef3e29ff691ab69c24f21
Gourav <gourav@tepia.co>
2024-05-10 10:57:22 +0530

    conflicts resolved

--- index.js
+++ index.js
@@ -25,24 +25,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = getFirestore();
 
-const firebase = require("firebase/app");
-require("firebase/firestore");
-const firestore = require("@firebase/firestore");
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firestore.getFirestore();
-
 const fs = require("fs");
 const path = require("path");
 
