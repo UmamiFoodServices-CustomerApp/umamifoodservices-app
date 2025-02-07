86d7d7c374f54d089b7ebc9e9ead8e609862c095
Gourav <gourav@tepia.co>
2025-02-07 17:36:45 +0530

    fix puppeteer issue

--- utils/order.js
+++ utils/order.js
@@ -1,8 +1,7 @@
 const moment = require("moment");
 const { default: puppeteer } = require("puppeteer");
-const fs = require('fs');
-const company = require('../service/vars');
-
+const fs = require("fs");
+const company = require("../service/vars");
 
 const formatMoney = (amount, precision = true) => {
   if (precision) {
@@ -82,35 +81,34 @@ const getItemFinalAmount = (item) =>
   (item?.primaryQuantity ?? 1) * (item?.CustomerPrice ?? 1) +
   (item?.secondaryQuantity ?? 0) * (item?.CustomerUnitPrice ?? 1);
 
-const getCaseItemName = item => {
-  const BrandName = item?.Brand || ''
-  const ItemName = item?.Name || ''
-  const itemCase = item?.primaryQuantity || 0
+const getCaseItemName = (item) => {
+  const BrandName = item?.Brand || "";
+  const ItemName = item?.Name || "";
+  const itemCase = item?.primaryQuantity || 0;
 
-  return `${BrandName} - ${ItemName} - Cs:${itemCase}`
-}
- const getUnitItemName = item => {
-  const BrandName = item?.Brand || ''
-  const ItemName = item?.Name || ''
-  const itemUnit = item?.secondaryQuantity || 0
+  return `${BrandName} - ${ItemName} - Cs:${itemCase}`;
+};
+const getUnitItemName = (item) => {
+  const BrandName = item?.Brand || "";
+  const ItemName = item?.Name || "";
+  const itemUnit = item?.secondaryQuantity || 0;
 
-  return `${BrandName} - ${ItemName} - Ut:${itemUnit}`
-}
+  return `${BrandName} - ${ItemName} - Ut:${itemUnit}`;
+};
 
- const applyDiscount = props => {
-  const now = new Date()
-  const start = new Date(props.OfferStartDate)
-  const end = new Date(props.OfferEndDate)
+const applyDiscount = (props) => {
+  const now = new Date();
+  const start = new Date(props.OfferStartDate);
+  const end = new Date(props.OfferEndDate);
   if (now >= start && now <= end) {
-    const discountMultiplier = (100 - Number(props.Discount)) / 100
-    const discountedPrice = Number(props.CustomerPrice) * discountMultiplier
-    return Number(Number(props.CustomerPrice) - discountedPrice).toFixed(2)
+    const discountMultiplier = (100 - Number(props.Discount)) / 100;
+    const discountedPrice = Number(props.CustomerPrice) * discountMultiplier;
+    return Number(Number(props.CustomerPrice) - discountedPrice).toFixed(2);
   }
-  return 0
-}
+  return 0;
+};
 
-
- const calculateTotalCost = (order) => {
+const calculateTotalCost = (order) => {
   let cost = order.items.reduce((total, item) => {
     const primaryQuantity = Number(item?.primaryQuantity || 0);
     const customerPrice = Number(item?.CustomerPrice || 0);
@@ -120,71 +118,110 @@ const getCaseItemName = item => {
     const totalCustomerPrice = primaryQuantity * customerPrice;
     const totalUnitPrice = secondaryQuantity * customerUnitPrice;
 
-    const discountCustomerPrice = applyDiscount({ ...item, CustomerPrice: totalCustomerPrice }) ?? 0;
-    const discountUnitPrice = applyDiscount({ ...item, CustomerPrice: totalUnitPrice }) ?? 0;
+    const discountCustomerPrice =
+      applyDiscount({ ...item, CustomerPrice: totalCustomerPrice }) ?? 0;
+    const discountUnitPrice =
+      applyDiscount({ ...item, CustomerPrice: totalUnitPrice }) ?? 0;
 
-    return total + (totalCustomerPrice - discountCustomerPrice) + (totalUnitPrice - discountUnitPrice);
+    return (
+      total +
+      (totalCustomerPrice - discountCustomerPrice) +
+      (totalUnitPrice - discountUnitPrice)
+    );
   }, 0);
 
   const discount = order?.discount ? (cost * order.discount) / 100 : 0;
   return cost - discount;
 };
 
-
 const getDeliveryTime = (
   date = moment.unix(),
   inputFormat = undefined,
-  outputFormat = 'MM/DD/YYYY'
+  outputFormat = "MM/DD/YYYY"
 ) => {
   if (date?.seconds) {
-    return moment(date.seconds * 1000).format('LL')
+    return moment(date.seconds * 1000).format("LL");
   }
   if (moment(date).year() > 2010) {
-    return moment(date, inputFormat).format(outputFormat)
+    return moment(date, inputFormat).format(outputFormat);
   }
-  return moment(date * 1000).format(outputFormat)
-}
+  return moment(date * 1000).format(outputFormat);
+};
 
 async function generatePdf(order, outputPath) {
-
-  const itemRows = order.items.map((item) => `
-  ${item.primaryQuantity > 0 ? `
+  const itemRows = order.items
+    .map(
+      (item) => `
+  ${
+    item.primaryQuantity > 0
+      ? `
     <div style="display: flex; flex-direction: row; color: #4a4a4a; font-family: Helvetica; padding: 6px 0;">
-      <div style="width: 10%; text-align: center;">${parseInt(item.primaryQuantity) || 0}</div>
+      <div style="width: 10%; text-align: center;">${
+        parseInt(item.primaryQuantity) || 0
+      }</div>
       <div style="width: 15%; text-align: center;">Case</div>
       <div style="width: 50%; text-align: left;">${getCaseItemName(item)}</div>
       <div style="width: 15%; text-align: center;">-</div>
-      <div style="width: 15%; text-align: center;">${formatMoney(item.CustomerPrice)}</div>
-      <div style="width: 20%; text-align: center; font-weight: bold;">${formatMoney(item.primaryQuantity * item.CustomerPrice)}</div>
+      <div style="width: 15%; text-align: center;">${formatMoney(
+        item.CustomerPrice
+      )}</div>
+      <div style="width: 20%; text-align: center; font-weight: bold;">${formatMoney(
+        item.primaryQuantity * item.CustomerPrice
+      )}</div>
     </div>
-  ` : ''}
+  `
+      : ""
+  }
 
-  ${item.secondaryQuantity > 0 ? `
+  ${
+    item.secondaryQuantity > 0
+      ? `
     <div style="display: flex; flex-direction: row; color: #4a4a4a; font-family: Helvetica; padding: 6px 0;">
-      <div style="width: 10%; text-align: center;">${parseInt(item.secondaryQuantity) || 0}</div>
+      <div style="width: 10%; text-align: center;">${
+        parseInt(item.secondaryQuantity) || 0
+      }</div>
       <div style="width: 15%; text-align: center;">Unit</div>
       <div style="width: 50%; text-align: left;">${getUnitItemName(item)}</div>
-      <div style="width: 15%; text-align: center;">${item.SoldByUnit ? item.Unit : '-'}</div>
-      <div style="width: 15%; text-align: center;">${formatMoney(item.CustomerUnitPrice)}</div>
-      <div style="width: 20%; text-align: center; font-weight: bold;">${formatMoney(item.secondaryQuantity * item.CustomerUnitPrice)}</div>
+      <div style="width: 15%; text-align: center;">${
+        item.SoldByUnit ? item.Unit : "-"
+      }</div>
+      <div style="width: 15%; text-align: center;">${formatMoney(
+        item.CustomerUnitPrice
+      )}</div>
+      <div style="width: 20%; text-align: center; font-weight: bold;">${formatMoney(
+        item.secondaryQuantity * item.CustomerUnitPrice
+      )}</div>
     </div>
-  ` : ''}
-`).join('');
-  const imagePath = './images/Logo.png';  
- const imageBase64 = fs.readFileSync(imagePath, { encoding: 'base64' });
-  
-  
-const htmlContent = `
+  `
+      : ""
+  }
+`
+    )
+    .join("");
+  const imagePath = "./images/Logo.png";
+  const imageBase64 = fs.readFileSync(imagePath, { encoding: "base64" });
+
+  const htmlContent = `
 <div id="umani-app-invoice-form-page-1" style="font-family: Helvetica, Arial, sans-serif; margin-top: 0.75rem; display: flex; justify-content: center; align-items: center; height: auto;">
   <div style="display: flex; flex-direction: row; justify-content: space-between; align-items: center; border-radius: 8px; padding: 0.5rem 1rem; width: 100%; max-width: 600px;">
     
   <img src="data:image/png;base64,${imageBase64}" width="90" height="90" style="display: block;" />
 
     <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; font-size: 14px; text-align: center;">
-      <p style="font-weight: bold; font-family: Helvetica-Bold; font-size: 28px; letter-spacing: 0.05em; margin: 0;">${company.name}</p>
-      <p style="font-size: 14px; font-family: Helvetica; margin: 2px 0;">${company.address}, ${company.city}, ${company.state} ${company.zip}, ${company.country}</p>
-      <p style="font-size: 14px; font-family: Helvetica; margin: 2px 0;">Tel: ${company.phone}  Email: ${company.email}</p>
-      <p style="font-size: 14px; font-family: Helvetica; margin: 2px 0;">Website: ${company.website}</p>
+      <p style="font-weight: bold; font-family: Helvetica-Bold; font-size: 28px; letter-spacing: 0.05em; margin: 0;">${
+        company.name
+      }</p>
+      <p style="font-size: 14px; font-family: Helvetica; margin: 2px 0;">${
+        company.address
+      }, ${company.city}, ${company.state} ${company.zip}, ${
+    company.country
+  }</p>
+      <p style="font-size: 14px; font-family: Helvetica; margin: 2px 0;">Tel: ${
+        company.phone
+      }  Email: ${company.email}</p>
+      <p style="font-size: 14px; font-family: Helvetica; margin: 2px 0;">Website: ${
+        company.website
+      }</p>
     </div>
 
     <p style="font-size: 20px; font-weight: bold; font-family: Helvetica-Bold; text-align: center; margin: 0;">INVOICE</p>
@@ -207,9 +244,17 @@ const htmlContent = `
       <p style="margin: 0;">Due Date:</p>
     </div>
     <div style="text-align: right; display: flex; flex-direction: column; gap: 0.4rem;">
-      <p style="font-weight: bold; font-family: Helvetica-Bold; color: #1a202c; margin: 0;">${order?.orderId ?? '-'}</p>
-      <p style="margin: 0;">${order?.deliveryDateTimestamp ? getDeliveryTime(order.deliveryDateTimestamp) : 'NA'}</p>
-      <p style="margin: 0;">${order?.dueDateTimestamp ? getDeliveryTime(order.dueDateTimestamp) : 'NA'}</p>
+      <p style="font-weight: bold; font-family: Helvetica-Bold; color: #1a202c; margin: 0;">${
+        order?.orderId ?? "-"
+      }</p>
+      <p style="margin: 0;">${
+        order?.deliveryDateTimestamp
+          ? getDeliveryTime(order.deliveryDateTimestamp)
+          : "NA"
+      }</p>
+      <p style="margin: 0;">${
+        order?.dueDateTimestamp ? getDeliveryTime(order.dueDateTimestamp) : "NA"
+      }</p>
     </div>
   </div>
 </div>
@@ -237,7 +282,9 @@ const htmlContent = `
       <p style="font-weight: bold; font-family: Helvetica-Bold; color: #000;">BALANCE DUE</p>
     </div>
     <div style="text-align: left;">
-      <p style="font-weight: bold; font-family: Helvetica-Bold; color: #000;">${formatMoney(calculateTotalCost(order))}</p>
+      <p style="font-weight: bold; font-family: Helvetica-Bold; color: #000;">${formatMoney(
+        calculateTotalCost(order)
+      )}</p>
     </div>
   </div>
 </div>
@@ -276,27 +323,27 @@ break-inside: avoid;
 </div>
 </div>
 </div>
-`
+`;
 
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  const page = await browser.newPage();
 
-await page.setContent(htmlContent, { waitUntil: 'domcontentloaded' });
-const options = {
-  path: outputPath, 
-  format: 'A4', 
-  printBackground: true, 
-  margin: {
-    top: '20mm',
-    right: '20mm',
-    bottom: '20mm',
-    left: '20mm',
-  },
-};
+  await page.setContent(htmlContent, { waitUntil: "domcontentloaded" });
+  const options = {
+    path: outputPath,
+    format: "A4",
+    printBackground: true,
+    margin: {
+      top: "20mm",
+      right: "20mm",
+      bottom: "20mm",
+      left: "20mm",
+    },
+  };
 
-await page.pdf(options);
+  await page.pdf(options);
 
-await browser.close();
+  await browser.close();
 }
 
 module.exports = {
@@ -309,5 +356,5 @@ module.exports = {
   getDeliveryTime,
   getUnitItemName,
   getCaseItemName,
-  generatePdf
+  generatePdf,
 };
