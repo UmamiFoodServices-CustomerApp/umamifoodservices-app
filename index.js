f91d3e57400e26c4ce88a0bba0ace0b8f3902444
Adeel Ahmad Rao <adeelrao007@gmail.com>
2025-09-05 13:09:13 +0500

    merging staging announcement backend module to prod

--- index.js
+++ index.js
@@ -74,6 +74,82 @@ const { paymentsApi, ordersApi, locationsApi, customersApi } = defaultClient;
 
 app.use(cors());
 
+// periodically check for the new scheduled messages (every minute)
+setInterval(async () => {
+  const systemMessagesCollection = db.collection("systemMessages")
+  const usersCollection = db.collection("users")
+  const systemAnnouncementsCollection = db.collection("systemAnnouncements")
+  const systemTextMessagesCollection = db.collection("systemTextMessages")
+  const scheduledMessages = await systemMessagesCollection.get()
+  console.log('Preparing to send scheduled messages if any')
+  if (!scheduledMessages.empty) {
+    scheduledMessages.docs.forEach(async (doc) => {
+
+      const messageData = doc.data();
+      const date = messageData.createdAt.toDate();
+      const time = messageData.time + ':00';
+      const timezoneOffset = new Date(date).getTimezoneOffset()
+
+      const offset = timezoneOffset / 60;
+      var scheduleDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), ...time.split(':').map(t => parseInt(t)));
+      scheduleDate.setHours(scheduleDate.getHours() - offset);
+      const currentDateTime = new Date()
+
+      if (scheduleDate.getTime() <= currentDateTime.getTime()) {
+        const docRef = doc.ref
+        await docRef.update({
+          ...doc.data(),
+          status: 'sent'
+        })
+
+        const customers = await usersCollection.where('receiveAnnouncements', '==', true).get();
+
+        if (!customers.empty) {
+          customers.docs.forEach(async (customerDoc) => {
+            const customerData = customerDoc.data();
+
+            const userDocAnnouncementData = {
+              systemMessageId: doc.id,
+              customerId: customerDoc.id,
+              message: messageData.message,
+              subject: messageData.subject,
+              status: 'un-read',
+              subject: messageData.subject
+            };
+            const existingAnnouncements = await systemAnnouncementsCollection
+              .where('systemMessageId', '==', doc.id)
+              .where('customerId', '==', customerDoc.id)
+              .get()
+            if (existingAnnouncements.empty) {
+              await systemAnnouncementsCollection.add(userDocAnnouncementData)
+            }
+
+            if (customerData.phone) {
+              const userDocTextData = {
+                systemMessageId: doc.id,
+                customerId: customerDoc.id,
+                phone: customerData.phone,
+                message: messageData.message,
+                subject: messageData.subject,
+                status: 'sent'
+              };
+              const existingTextMessage = await systemTextMessagesCollection
+                .where('systemMessageId', '==', doc.id)
+                .where('customerId', '==', customerDoc.id)
+                .get()
+              if (existingTextMessage.empty) {
+                await systemTextMessagesCollection.add(userDocTextData)
+              }
+            } else {
+              console.log('customerPhoneDoesNotExist', customerDoc.id, customerData)
+            }
+          });
+        }
+      }
+    });
+  }
+}, 60000);
+
 app.post(
   "/sendPasswordReset",
   bodyParser.urlencoded({ extended: false }),
@@ -775,6 +851,7 @@ app.post(
         state,
         city,
         zipCode,
+        receiveAnnouncements,
       } = request.body;
 
       const missingFields = [];
@@ -788,6 +865,7 @@ app.post(
       if (!state) missingFields.push("state");
       if (!city) missingFields.push("city");
       if (!zipCode) missingFields.push("zipCode");
+      if (!receiveAnnouncements) missingFields.push("receiveAnnouncements");
 
       if (missingFields.length > 0) {
         return res.status(400).send({
@@ -823,6 +901,7 @@ app.post(
         state,
         zip: zipCode,
         businessName: fullName,
+        receiveAnnouncements
       });
 
       await db.collection("users").doc(user.uid).set(userDocData);
