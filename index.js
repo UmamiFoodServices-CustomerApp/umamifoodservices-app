74983463db6510173f57fa6f33620fb82562a10e
Gourav <gourav@tepia.co>
2025-09-09 19:08:34 +0530

    added send announcement sms feature

--- index.js
+++ index.js
@@ -72,95 +72,6 @@ const defaultClient = new Client({
 
 const { paymentsApi, ordersApi, locationsApi, customersApi } = defaultClient;
 
-// periodically check for the new scheduled messages (every minute)
-setInterval(async () => {
-  const systemMessagesCollection = db.collection("systemMessages")
-  const usersCollection = db.collection("users")
-  const systemAnnouncementsCollection = db.collection("systemAnnouncements")
-  const systemTextMessagesCollection = db.collection("systemTextMessages")
-  const scheduledMessages = await systemMessagesCollection.get()
-  console.log('Preparing to send scheduled messages if any')
-  if (!scheduledMessages.empty) {
-    scheduledMessages.docs.forEach(async (doc) => {
-
-      const messageData = doc.data();
-      const date = messageData.createdAt.toDate();
-      const time = messageData.time + ':00';
-      const timezoneOffset = new Date(date).getTimezoneOffset()
-
-      const offset = timezoneOffset / 60;
-      let scheduleDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), ...time.split(':').map(t => parseInt(t)));
-      scheduleDate.setHours(scheduleDate.getHours() - offset);
-      const currentDateTime = new Date()
-
-      if (scheduleDate.getTime() <= currentDateTime.getTime()) {
-        const docRef = doc.ref
-        await docRef.update({
-          ...doc.data(),
-          status: 'sent'
-        })
-
-        const customers = await usersCollection.where('receiveAnnouncements', '==', true).get();
-        
-        if (!customers.empty) {
-          customers.docs.forEach(async (customerDoc) => {
-            const customerData = customerDoc.data();
-
-            const userDocAnnouncementData = {
-              systemMessageId: doc.id,
-              customerId: customerDoc.id,
-              message: messageData.message,
-              subject: messageData.subject,
-              status: 'un-read'
-            };
-            const existingAnnouncements = await systemAnnouncementsCollection
-              .where('systemMessageId', '==', doc.id)
-              .where('customerId', '==', customerDoc.id)
-              .get()
-            if (existingAnnouncements.empty) {
-              await systemAnnouncementsCollection.add(userDocAnnouncementData)
-            }
-
-            if (customerData.phone) {
-              let customerPhoneNumber = customerData.phone
-              // replace all non-numeric characters
-              const cleanedNumber = customerPhoneNumber.replace(/[^\d]/g, "")
-              if (cleanedNumber.length === 11) {
-                customerPhoneNumber = "+" + cleanedNumber
-              } else if (cleanedNumber.length === 10 && !cleanedNumber.startsWith("1")) {
-                customerPhoneNumber = "+1" + cleanedNumber
-              } else {
-                customerPhoneNumber = null
-              }
-              if (customerPhoneNumber) {
-                const userDocTextData = {
-                  systemMessageId: doc.id,
-                  customerId: customerDoc.id,
-                  phone: customerPhoneNumber,
-                  message: messageData.message,
-                  subject: messageData.subject,
-                  status: 'sent'
-                };
-                const existingTextMessage = await systemTextMessagesCollection
-                  .where('systemMessageId', '==', doc.id)
-                  .where('customerId', '==', customerDoc.id)
-                  .get()
-                if (existingTextMessage.empty) {
-                  await systemTextMessagesCollection.add(userDocTextData)
-                }
-              } else {
-                console.log('InvalidPhoneNumber', customerDoc.id, customerData.phone)
-              }
-            } else {
-              console.log('customerPhoneDoesNotExist', customerDoc.id, customerData)
-            }
-          });
-        }
-      }
-    });
-  }
-}, 60000);
-
 app.use(cors());
 
 app.post(
@@ -1053,6 +964,7 @@ app.post(
 
 require("./controllers/email.controllers")(app, db, bodyParser);
 require("./webhook/herokuWebhook")(app);
+require("./src/announcements")({db});
 
 app.get("/migrateInternalCost", async (req, res) => {
   try {
