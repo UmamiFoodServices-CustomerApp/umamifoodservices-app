9454edb366b43a3ec7b2c4f8f41524827ce5c4b0
Gourav <gourav@tepia.co>
2025-12-08 10:29:50 +0530

    [43587] Periodically checking HTTPS codes

--- index.js
+++ index.js
@@ -1144,6 +1144,10 @@ app.post(
   }
 );
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Umami Food Services API Server!");
+});
+
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
